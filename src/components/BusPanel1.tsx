6d07f6a0cc8efe65d4c7c9b635629cf468b6e73e
stevebarakat <stevebarakat@gmail.com>
2023-05-14 06:19:16 -0400

    checkin

---
+++ src/components/BusPanel1.tsx
@@ -0,0 +1,110 @@
+import { array } from "../utils";
+import { Rnd } from "react-rnd";
+import CloseButton from "./Buttons/CloseButton";
+import Reverber from "./Fx/Reverber";
+import Delay from "./Fx/Delay";
+import { MixerMachineContext } from "../App";
+import type { FeedbackDelay, Reverb } from "tone";
+
+type Props = {
+  disabled: { panel1: boolean; panel2: boolean };
+  busFx: {
+    reverb1: Reverb;
+    reverb2: Reverb;
+    delay1: FeedbackDelay;
+    delay2: FeedbackDelay;
+  };
+  fx: React.MutableRefObject<{
+    reverb1: Reverb;
+    reverb2: Reverb;
+    delay1: FeedbackDelay;
+    delay2: FeedbackDelay;
+  }>;
+};
+
+function BusPanel1({ busFx, fx, disabled }: Props) {
+  const { send } = MixerMachineContext.useActorRef();
+
+  console.log("fx", fx);
+  console.log("busFx", busFx);
+
+  const bpOpen0 = MixerMachineContext.useSelector(
+    (state) => state.context.busPanelsOpen[0]
+  );
+
+  const bpPos0 = MixerMachineContext.useSelector(
+    (state) => state.context.busPanelsPosition[0]
+  );
+
+  const bpSize0 = MixerMachineContext.useSelector(
+    (state) => state.context.busPanelsSize[0]
+  );
+
+  return (
+    <div>
+      {bpOpen0 && !disabled.panel1 && (
+        <Rnd
+          className="fx-panel"
+          position={bpPos0}
+          onDragStop={(_, d) => {
+            send({
+              type: "SAVE_BUS_PANELS_POSITION",
+              busIndex: 0,
+              position: { x: d.x, y: d.y },
+            });
+          }}
+          size={bpSize0}
+          minWidth="200px"
+          onResizeStop={(_, __, ref) => {
+            send({
+              type: "SAVE_BUS_PANELS_SIZE",
+              busIndex: 0,
+              size: { width: ref.style.width, height: ref.style.height },
+            });
+          }}
+          cancel="input"
+        >
+          <CloseButton
+            id="bus-panel-1"
+            onClick={() => {
+              send({
+                type: "TOGGLE_BUS_PANEL",
+                busIndex: 0,
+              });
+            }}
+          >
+            X
+          </CloseButton>
+
+          {array(2).map((_, i) => {
+            switch (busFx[`bus1fx${i + 1}`]) {
+              case "reverb1":
+                return (
+                  <Reverber
+                    key={`bus1reverb${i}`}
+                    reverb={fx.current.reverb1}
+                    busIndex={0}
+                    fxIndex={0}
+                  />
+                );
+              case "delay1":
+                return (
+                  <Delay
+                    key={`bus1delay${i}`}
+                    delay={fx.current.delay1}
+                    busIndex={0}
+                    fxIndex={0}
+                  />
+                );
+              default:
+                break;
+            }
+            return null;
+          })}
+        </Rnd>
+      )}
+    </div>
+  );
+}
+
+export default BusPanel1;
--- src/components/BusPanels.tsx
+++ src/components/BusPanels.tsx
@@ -1,169 +1,23 @@
-import { array } from "../utils";
-import { Rnd } from "react-rnd";
-import CloseButton from "./Buttons/CloseButton";
-import Reverber from "./Fx/Reverber";
-import Delay from "./Fx/Delay";
-import { MixerMachineContext } from "../App";
 import { FeedbackDelay, Reverb } from "tone";
+import BusPanel1 from "./BusPanel1";
+import BusPanel2 from "./BusPanel2";
 
 type Props = {
   fx: any;
-  busFx: Reverb | FeedbackDelay;
+  busFx: {
+    reverb1: Reverb;
+    reverb2: Reverb;
+    delay1: FeedbackDelay;
+    delay2: FeedbackDelay;
+  };
   disabled: { panel1: boolean; panel2: boolean };
 };
 
 function BusPanels({ fx, busFx, disabled }: Props) {
-  const { send } = MixerMachineContext.useActorRef();
-  const bpOpen0 = MixerMachineContext.useSelector(
-    (state) => state.context.busPanelsOpen[0]
-  );
-  const bpOpen1 = MixerMachineContext.useSelector(
-    (state) => state.context.busPanelsOpen[1]
-  );
-
-  const bpPos0 = MixerMachineContext.useSelector(
-    (state) => state.context.busPanelsPosition[0]
-  );
-  const bpPos1 = MixerMachineContext.useSelector(
-    (state) => state.context.busPanelsPosition[1]
-  );
-
-  const bpSize0 = MixerMachineContext.useSelector(
-    (state) => state.context.busPanelsSize[0]
-  );
-  const bpSize1 = MixerMachineContext.useSelector(
-    (state) => state.context.busPanelsSize[1]
-  );
-
   return (
     <div>
-      {bpOpen0 && !disabled.panel1 ? (
-        <Rnd
-          className="fx-panel"
-          position={bpPos0}
-          onDragStop={(_, d) => {
-            send({
-              type: "SAVE_BUS_PANELS_POSITION",
-              busIndex: 0,
-              position: { x: d.x, y: d.y },
-            });
-          }}
-          size={bpSize0}
-          minWidth="200px"
-          onResizeStop={(_, __, ref) => {
-            send({
-              type: "SAVE_BUS_PANELS_SIZE",
-              busIndex: 0,
-              size: { width: ref.style.width, height: ref.style.height },
-            });
-          }}
-          cancel="input"
-        >
-          <CloseButton
-            id="bus-panel-1"
-            onClick={() => {
-              send({
-                type: "TOGGLE_BUS_PANEL",
-                busIndex: 0,
-              });
-            }}
-          >
-            X
-          </CloseButton>
-
-          {array(2).map((_, i) => {
-            let currentFx: ReactNode | null = null;
-            switch (busFx[`bus1fx${i + 1}`]) {
-              case "reverb1":
-                currentFx = (
-                  <Reverber
-                    key={`bus1reverb${i}`}
-                    reverb={fx.current.reverb1}
-                    busIndex={0}
-                    fxIndex={0}
-                  />
-                );
-                break;
-              case "delay1":
-                currentFx = (
-                  <Delay
-                    key={`bus1delay${i}`}
-                    delay={fx.current.delay1}
-                    busIndex={0}
-                    fxIndex={0}
-                  />
-                );
-                break;
-              default:
-                break;
-            }
-            return currentFx;
-          })}
-        </Rnd>
-      ) : (
-        "ubu!"
-      )}
-      {bpOpen1 && !disabled.panel2 && (
-        <Rnd
-          className="fx-panel"
-          position={bpPos1}
-          onDragStop={(_, d) => {
-            send({
-              type: "SAVE_BUS_PANELS_POSITION",
-              busIndex: 1,
-              position: { x: d.x, y: d.y },
-            });
-          }}
-          size={bpSize1}
-          minWidth="200px"
-          onResizeStop={(_, __, ref) => {
-            send({
-              type: "SAVE_BUS_PANELS_SIZE",
-              busIndex: 1,
-              size: { width: ref.style.width, height: ref.style.height },
-            });
-          }}
-          cancel="input"
-        >
-          <CloseButton
-            id="bus-panel-1"
-            onClick={() => {
-              send({
-                type: "TOGGLE_BUS_PANEL",
-                busIndex: 1,
-              });
-            }}
-          >
-            X
-          </CloseButton>
-
-          {array(2).map((_, i) => {
-            switch (busFx[`bus2fx${i + 1}`]) {
-              case "reverb2":
-                return (
-                  <Reverber
-                    key={`bus2reverb${i}`}
-                    reverb={fx.reverb2}
-                    busIndex={0}
-                    fxIndex={0}
-                  />
-                );
-              case "delay2":
-                return (
-                  <Delay
-                    key={`bus2delay${i}`}
-                    delay={fx.delay2}
-                    busIndex={0}
-                    fxIndex={0}
-                  />
-                );
-              default:
-                break;
-            }
-            return null;
-          })}
-        </Rnd>
-      )}
+      <BusPanel1 disabled={disabled} fx={fx} busFx={busFx} />
+      <BusPanel2 disabled={disabled} fx={fx} busFx={busFx} />
     </div>
   );
 }
